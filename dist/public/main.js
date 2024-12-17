f4afb10ff2fd9db14f5884259f70bdb16fa95e52
Massimo Melina <a@rejetto.com>
2024-12-17 23:05:45 +0100

    first commit

---
+++ dist/public/main.js
@@ -0,0 +1,39 @@
+"use strict";{
+    const { React } = HFS
+    const cfg = HFS.getPluginConfig()
+    const exts = cfg.extensions?.toLowerCase().split(',').map(x => x.trim())
+
+    HFS.onEvent('fileShow', ({ entry }) =>
+        exts.includes(entry.ext) && Video)
+
+    function Video(props) {
+        const [convert, setConvert] = React.useState(false)
+        React.useEffect(() => setConvert(false), [props.src])
+        const ref = React.useRef()
+        React.useEffect(() => {
+            const was = ref.current
+            return () => {
+                if (ref.current) return
+                // it was removed from the dom, now do this trick to cause a quicker termination of the request
+                was.removeAttribute('src')
+                was.load()
+            }
+        }, [])
+        return HFS.h('video', {
+            ...props,
+            ref,
+            onLoadedData: props.onLoad,
+            controls: true,
+            src: props.src + (convert ? '?ffmpeg' : ''),
+            onError(err) {
+                const mediaError = document.querySelector('.showing-container .showing')?.error?.code
+                if (!convert && mediaError >= 3) { // 3 and 4 = decoding errors
+                    setConvert(true)
+                    HFS.toast("converting unsupported video format", 'success')
+                }
+                else
+                    props.onError?.(err)
+            },
+        })
+    }
+}
